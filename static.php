be5c654d756f7b9f6b7220603786e009820c4703
Sebastian <sebastian@bohemiaamsterdam.com>
2018-05-07 13:02:52 +0200

    Initial commit
    Static page

---
+++ static.php
@@ -0,0 +1,235 @@
+<?php /* Template Name: Static Homepage */ ?>
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link href="https://fonts.googleapis.com/css?family=Rakkas" rel="stylesheet">
+	<title>KrachtKern</title>
+	<style>
+		/* http://meyerweb.com/eric/tools/css/reset/
+		   v2.0 | 20110126
+		   License: none (public domain)
+		*/
+
+		html, body, div, span, applet, object, iframe,
+		h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+		a, abbr, acronym, address, big, cite, code,
+		del, dfn, em, img, ins, kbd, q, s, samp,
+		small, strike, strong, sub, sup, tt, var,
+		b, u, i, center,
+		dl, dt, dd, ol, ul, li,
+		fieldset, form, label, legend,
+		table, caption, tbody, tfoot, thead, tr, th, td,
+		article, aside, canvas, details, embed,
+		figure, figcaption, footer, header, hgroup,
+		menu, nav, output, ruby, section, summary,
+		time, mark, audio, video {
+			margin: 0;
+			padding: 0;
+			border: 0;
+			font-size: 100%;
+			font: inherit;
+			vertical-align: baseline;
+		}
+		/* HTML5 display-role reset for older browsers */
+		article, aside, details, figcaption, figure,
+		footer, header, hgroup, menu, nav, section {
+			display: block;
+		}
+		body {
+			line-height: 1;
+		}
+		ol, ul {
+			list-style: none;
+		}
+		blockquote, q {
+			quotes: none;
+		}
+		blockquote:before, blockquote:after,
+		q:before, q:after {
+			content: '';
+			content: none;
+		}
+		table {
+			border-collapse: collapse;
+			border-spacing: 0;
+		}
+
+		/* Custom */
+		html, body { min-height: 100%; }
+		body {
+			background-image: url("<?php bloginfo('stylesheet_directory'); ?>/images/bg@2x.png");
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-position: bottom center;
+
+		}
+		iframe {
+			width: 100vw;
+			height: 100vh;
+		}
+		#logos {
+			float: right;
+			margin: 20px 20px 15px 0;
+			vertical-align: middle;
+			display: block;
+		}
+		.logo {
+			display: inline-block;
+		}
+		#logos .logo {
+			width: 100px;
+		}
+		#logos #plus {
+			width:15px;
+			margin: 0 5px 12px;
+		}
+
+		@media (min-width: 768px) {
+		}
+
+		@media (min-width: 992px) {
+			body {
+				background-position: bottom right;
+			}
+		}
+	</style>
+</head>
+<body>
+	<div id="logos">
+    	<img class="logo" style="margin-bottom: 12px;" src="<?php bloginfo('stylesheet_directory'); ?>/images/aat_logo.svg" alt="Aat Vos"/>
+		<img id="plus" class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/plus.svg" alt=""/>
+		<img class="logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/tah_logo.svg" alt="The Alignment House"/>
+	</div>
+	<div style="clear: both"></div>
+	<div class="container" style="max-width: 700px">
+  		<div width="100%" class="mcnTextBlock" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+			<div class="mcnTextBlockInner" style="display: flex;padding: 9px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+
+				<div style="width: 100%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+					<div class="mcnTextContent" style="padding-top: 0;padding-right: 9px;padding-bottom: 9px;padding-left: 9px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;word-break: break-word;flex-flow: column;color: #808080;font-family: 'Rakkas', sans-serif;font-size: 16px;line-height: 150%;text-align: left;box-sizing: border-box !important;">
+						<h1 style="text-align: left;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;display: block;margin: 0;padding: 0;color: #222222;font-family: 'Rakkas', sans-serif;font-size: 40px;font-style: normal;font-weight: bold;line-height: 150%;letter-spacing: normal;box-sizing: border-box !important;"><span style="color: #001EC4;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="font-size: 64px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><strong style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="font-family: 'Rakkas', sans-serif;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">Hallo,</span></strong></span></span></h1>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div width="100%" class="mcnTextBlock" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+			<div class="mcnTextBlockInner" style="display: flex;padding: 9px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+
+				<div style="width: 100%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+					<div class="mcnTextContent" style="padding: 0px 9px 9px;line-height: 200%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;word-break: break-word;flex-flow: column;color: #808080;font-family: Helvetica;font-size: 16px;text-align: left;box-sizing: border-box !important;">
+						<p style="line-height: 200%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;margin: 10px 0;padding: 0;color: #808080;font-family: Helvetica;font-size: 16px;text-align: left;box-sizing: border-box !important;"><span style="font-size: 32px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="color: #000000;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">Ben je ook nieuwsgierig naar wat </span><strong style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="color: #001EC4;font-weight:700;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">KrachtKern</span></strong><span style="color: #000000;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"> voor jou kan betekenen?</span></span></p>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div width="100%" class="mcnTextBlock" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+			<div class="mcnTextBlockInner" style="display: flex;padding: 9px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+
+				<div style="width: 100%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+					<div class="mcnTextContent" style="padding: 0px 9px 9px;line-height: 150%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;word-break: break-word;flex-flow: column;color: #808080;font-family: Helvetica;font-size: 16px;text-align: left;box-sizing: border-box !important;">
+						<p style="line-height: 150%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;margin: 10px 0;padding: 0;color: #808080;font-family: Helvetica;font-size: 16px;text-align: left;box-sizing: border-box !important;"><span style="font-size: 18px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="color: #000000;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">Neem contact op met <a href="mailto:info@alignmenthouse.nl" target="_blank" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;color: #001ec4;font-weight: normal;text-decoration: underline;box-sizing: border-box !important;">The Alignment House</a> of <a href="mailto:aat@aatvos.com" target="_blank" style="-webkit-box-sizing: border-box;-moz-box-sizing: border-box;color: #001ec4;font-weight: normal;text-decoration: underline;box-sizing: border-box !important;">Aat Vos</a>.</span></span></p>
+
+		<p style="line-height: 150%;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;margin: 10px 0;padding: 0;color: #808080;font-family: Helvetica;font-size: 16px;text-align: left;box-sizing: border-box !important;"><span style="font-size: 18px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;"><span style="color: #000000;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">Of laat jouw gegevens achter en ontvang de PDF van de presentatie die we gegeven hebben op het Nationale Bibliotheek Congres.</span></span></p>
+
+					</div>
+				</div>
+
+			</div>
+		</div>
+		<div style="display: flex;align-items: center;justify-content: center;overflow: hidden;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+			<div style="flex: auto;align-items: center;justify-content: center;padding-top: 27px;-webkit-box-sizing: border-box;-moz-box-sizing: border-box;box-sizing: border-box !important;">
+				<!-- Begin MailChimp Signup Form -->
+				<link href="//cdn-images.mailchimp.com/embedcode/classic-10_7.css" rel="stylesheet" type="text/css">
+				<style type="text/css">
+					#mc_embed_signup{clear:left; font:14px Helvetica,Arial,sans-serif; }
+					.input-group strong { width: 100%;}
+					#mc_embed_signup #mc-embedded-subscribe-form div.mce_inline_error {
+						background: none;
+						padding: 0;
+					}
+					input[type="text"], input[type="email"] { height: 3em;}
+					#mc_embed_signup .button {
+						width: 100%;
+						background-color: rgb(0, 30, 196);
+						border-radius: 3px;
+						text-align: center;
+						text-transform: none;
+						vertical-align: middle;
+						font-weight: 700;
+						height: 3em;
+					}
+					#mc_embed_signup .button:hover {
+						background-color: #031a8e;
+					}
+
+					input[type="radio"], input[type="checkbox"] {
+						float: left;
+						margin-right: 10px;
+						position: relative;
+						top: 3px;
+					}
+					#mc_embed_signup .mc-field-group label {
+						font-weight: 700;
+						font-size: 15px;
+					}
+					#mc_embed_signup div#mce-responses {
+						margin: 0;
+						padding: 0;
+					}
+					#mc_embed_signup #mce-success-response {
+						padding: 0;
+						margin-top: 0;
+						margin-bottom: 2em;
+					}
+				</style>
+				<div id="mc_embed_signup">
+				<form action="https://aatvos.us7.list-manage.com/subscribe/post?u=0837d9e51a7ef903f9de37857&amp;id=4f101d69f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+					<div id="mc_embed_signup_scroll">
+				<div class="mc-field-group">
+					<label for="mce-EMAIL">Mail adres </label>
+					<input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
+				</div>
+				<div class="mc-field-group">
+					<label for="mce-FNAME">Voornaam </label>
+					<input type="text" value="" name="FNAME" class="required" id="mce-FNAME">
+				</div>
+				<div class="mc-field-group">
+					<label for="mce-LNAME">Achternaam </label>
+					<input type="text" value="" name="LNAME" class="required" id="mce-LNAME">
+				</div>
+
+				<div class="mc-field-group">
+					<label for="mce-ORG">Organisatie </label>
+					<input type="text" value="" name="ORG" class="" id="mce-ORG">
+				</div>
+				<div class="mc-field-group input-group">
+					<label style="width:100%">Ik ben bekend met:</label>
+					<ul><li><input type="radio" value="Aat Vos" name="KENT" id="mce-KENT-0"><label for="mce-KENT-0">Aat Vos</label></li>
+				<li><input type="radio" value="The Alignment House" name="KENT" id="mce-KENT-1"><label for="mce-KENT-1">The Alignment House</label></li>
+				<li><input type="radio" value="Geen van beiden" name="KENT" id="mce-KENT-2"><label for="mce-KENT-2">Geen van beiden</label></li>
+				</ul>
+				</div>
+					<div id="mce-responses" class="clear">
+						<div class="response" id="mce-error-response" style="display:none"></div>
+						<div class="response" id="mce-success-response" style="display:none"></div>
+					</div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+					<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0837d9e51a7ef903f9de37857_4f101d69f0" tabindex="-1" value=""></div>
+					<div class="clear"><input type="submit" value="Stuur" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+					</div>
+				</form>
+				</div>
+				<script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script><script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';fnames[6]='ORG';ftypes[6]='text';fnames[0]='EMAIL';ftypes[0]='email';fnames[3]='KENT';ftypes[3]='radio';}(jQuery));var $mcj = jQuery.noConflict(true);</script>
+				<!--End mc_embed_signup-->
+			</div>
+		</div>
+	</div>
+</body>
+
+</html>

55c39df193280ee214a9059d444957c3f3652e90
Sebastian <sebastian@bohemiaamsterdam.com>
2018-05-31 15:32:59 +0200

    Ready for client review

--- functions.php
+++ functions.php
@@ -1,12 +1,21 @@
-<?php 
+<?php
 
 add_action( 'wp_enqueue_scripts', 'salient_child_enqueue_styles');
 function salient_child_enqueue_styles() {
-	
+
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css', array('font-awesome'));
 
-    if ( is_rtl() ) 
+    if ( is_rtl() )
    		wp_enqueue_style(  'salient-rtl',  get_template_directory_uri(). '/rtl.css', array(), '1', 'screen' );
 }
+
+function salient_redux_custom_fonts( $custom_fonts ) {
+    return array(
+        'Custom Fonts' => array(
+             'aaux-next' => "Aaux Next"
+        )
+    );
+}
+add_filter( "redux/salient_redux/field/typography/custom_fonts", "salient_redux_custom_fonts" );
 
 ?>
